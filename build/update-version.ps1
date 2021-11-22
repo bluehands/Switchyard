378fc790be67adb6812052b4b232aefb3e3320c3
Alexander Wiedemann <wiedemann@bluehands.de>
2021-11-22 08:56:35 +0100

    - adapt build

--- build/update-version.ps1
+++ build/update-version.ps1
@@ -1,8 +1,20 @@
-$version = $args[0]
-Write-Host "Set version: $version"
+function Set-VsixVersion {
+  Param(
+    [string]
+    $ManifestPath,
+    [string]
+    $Version
+  )
+  
+  $FullPath = Resolve-Path ("$PSScriptRoot\..\src\$ManifestPath")
+  Write-Host "Setting version to $Version in file $FullPath"
+  [xml]$content = Get-Content $FullPath
+  $content.PackageManifest.Metadata.Identity.Version = $Version
+  $content.Save($FullPath)
+}
 
-$FullPath = Resolve-Path $PSScriptRoot\..\src\Switchyard\source.extension.vsixmanifest
-Write-Host $FullPath
-[xml]$content = Get-Content $FullPath
-$content.PackageManifest.Metadata.Identity.Version = $version
-$content.Save($FullPath)
+$Version = $args[0]
+Write-Host "Set version: $Version"
+
+Set-VsixVersion -ManifestPath "Switchyard.Vsix\source.extension.vsixmanifest" -Version $Version
+Set-VsixVersion -ManifestPath "Switchyard.Vsix.22\source.extension.vsixmanifest" -Version $Version
