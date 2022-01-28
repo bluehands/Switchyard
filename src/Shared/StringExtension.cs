08b1b89c79f3f7a3d5c8fbce1f1d69b1da125a8d
Alexander Wiedemann <wiedemann@bluehands.de>
2022-01-28 23:55:17 +0100

    - safe FirstTo...

--- src/Shared/StringExtension.cs
+++ src/Shared/StringExtension.cs
@@ -2,7 +2,7 @@
 {
     public static class StringExtension
     {
-        public static string FirstToLower(this string name) => char.ToLowerInvariant(name[0]) + name.Substring(1);
-        public static string FirstToUpper(this string name) => char.ToUpperInvariant(name[0]) + name.Substring(1);
+        public static string FirstToLower(this string name) => string.IsNullOrEmpty(name) ? name : char.ToLowerInvariant(name[0]) + name.Substring(1);
+        public static string FirstToUpper(this string name) => string.IsNullOrEmpty(name) ? name : char.ToUpperInvariant(name[0]) + name.Substring(1);
     }
 }
